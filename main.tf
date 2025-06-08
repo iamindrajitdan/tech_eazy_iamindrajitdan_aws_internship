27521cca651a122eff8ff0ec6cf55ebf3503b7f6
iamindrajitdan <iamindrajitdan@gmail.com>
2025-06-08 21:03:38 +0530

    assignment1

--- main.tf
+++ main.tf
@@ -97,6 +97,7 @@ resource "aws_instance" "app_server" {
   iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
   subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.app_sg.id]
+  associate_public_ip_address = true
   user_data              = templatefile("${path.module}/user_data.sh.tftpl", {
     stage     = var.stage
     repo_url  = var.repo_url
